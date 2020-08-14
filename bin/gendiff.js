c2bc58b85105d3f4124e7d2fe014ea3d8ffbf3db
Oleg Kuzmenko <oleg.v.kuzmenko@gmail.com>
2020-08-14 15:01:32 +0300

    step 2 done

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,9 +5,10 @@ import commander from 'commander';
 const program = new commander.Command();
 
 program
-  .name('gendiff')
-  .usage('[options]')
   .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format ')
+  .helpOption('-h, --help', 'output usage information');
 
 program.parse(process.argv);
