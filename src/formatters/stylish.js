143c687fe18678545855370ffa587f84da9b12cc
Oleg Kuzmenko <oleg.v.kuzmenko@gmail.com>
2020-09-07 14:11:06 +0300

    refactoring 2

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -1,6 +1,6 @@
 import _ from 'lodash';
 
-const space = '  ';
+const space = '    ';
 
 const formatValue = (value, depth) => {
   if (!_.isObject(value)) {
@@ -8,7 +8,7 @@ const formatValue = (value, depth) => {
   }
   const formatedString = Object.entries(value).map(([name, innerValue]) => {
     const object = { name, value: innerValue };
-    return `${space.repeat(depth)}    ${object.name}: ${formatValue(object.value, depth + 2)}\n`;
+    return `${space.repeat(depth)}    ${object.name}: ${formatValue(object.value, depth + 1)}\n`;
   });
   return `{\n${formatedString.join('')}${space.repeat(depth)}}`;
 };
@@ -18,15 +18,15 @@ const stylish = (diff) => {
     const formatedString = tree.map((node) => {
       switch (node.type) {
         case 'modified':
-          return `${space.repeat(depth)}  - ${node.name}: ${formatValue(node.beforeValue, depth + 2)}\n${space.repeat(depth)}  + ${node.name}: ${formatValue(node.afterValue, depth + 2)}\n`;
+          return `${space.repeat(depth)}  - ${node.name}: ${formatValue(node.beforeValue, depth + 1)}\n${space.repeat(depth)}  + ${node.name}: ${formatValue(node.afterValue, depth + 1)}\n`;
         case 'unmodified':
-          return `${space.repeat(depth)}    ${node.name}: ${formatValue(node.value, depth + 2)}\n`;
+          return `${space.repeat(depth)}    ${node.name}: ${formatValue(node.value, depth + 1)}\n`;
         case 'deleted':
-          return `${space.repeat(depth)}  - ${node.name}: ${formatValue(node.value, depth + 2)}\n`;
+          return `${space.repeat(depth)}  - ${node.name}: ${formatValue(node.value, depth + 1)}\n`;
         case 'added':
-          return `${space.repeat(depth)}  + ${node.name}: ${formatValue(node.value, depth + 2)}\n`;
+          return `${space.repeat(depth)}  + ${node.name}: ${formatValue(node.value, depth + 1)}\n`;
         case 'nested':
-          return `${space.repeat(depth)}    ${node.name}: ${inner(node.children, depth + 2)}\n`;
+          return `${space.repeat(depth)}    ${node.name}: ${inner(node.children, depth + 1)}\n`;
         default:
           throw new Error(`Wrong type: ${node.type}`);
       }
