352fb2a51a0f0534924ed428982ff0588e8da46a
Oleg Kuzmenko <oleg.v.kuzmenko@gmail.com>
2020-09-14 12:52:01 +0300

    refactoring 5: update tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,25 +1,17 @@
-/* eslint-disable no-underscore-dangle */
 import fs from 'fs';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
-
+import path from 'path';
 import gendiff from '../src/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-const resultStylish = fs.readFileSync(`${__dirname}/../__fixtures__/result_stylish.txt`, 'utf-8');
-const resultPlain = fs.readFileSync(`${__dirname}/../__fixtures__/result_plain.txt`, 'utf-8');
-const resultJson = fs.readFileSync(`${__dirname}/../__fixtures__/result.json`, 'utf-8');
-const jsonBeforePath = '__fixtures__/before_nested.json';
-const jsonAfterPath = '__fixtures__/after_nested.json';
-const ymlAfterPath = '__fixtures__/after_nested.yml';
-const iniBeforePath = '__fixtures__/before_nested.ini';
+const getFixturePath = (filename) => path.join(process.cwd(), '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
 const testData = [
-  ['stylish', iniBeforePath, jsonAfterPath, resultStylish],
-  ['plain', jsonBeforePath, ymlAfterPath, resultPlain],
-  ['json', iniBeforePath, ymlAfterPath, resultJson],
+  ['stylish', 'before_nested.ini', 'after_nested.json', 'result_stylish.txt'],
+  ['plain', 'before_nested.json', 'after_nested.yml', 'result_plain.txt'],
+  ['json', 'before_nested.ini', 'after_nested.yml', 'result.json'],
 ];
 
 test.each(testData)('try %s formatter', (formatter, before, after, result) => {
-  expect(gendiff(before, after, formatter)).toEqual(result);
+  expect(gendiff(getFixturePath(before), getFixturePath(after), formatter))
+    .toEqual(readFile(result));
 });
